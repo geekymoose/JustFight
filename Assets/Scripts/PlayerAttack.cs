1bee052e8fc95947253667e71a550b95aff9f243
geekymoose <constantin.masson@umontreal.ca>
2021-01-12 14:19:31 +0100

    Create a simple projectile shot

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,18 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    [Tooltip("Projectile to throw")]
+    private GameObject projectile;
+
+    [SerializeField]
+    [Tooltip("This is where the projectil comes from")]
+    private Transform weaponEndPoint;
+
+    [SerializeField]
+    [Tooltip("Amount of power charged in one second (this is the loading speed)")]
+    private float chargingTimeInPowerPerSec = 0;
+
+    [SerializeField]
+    [Tooltip("Maximum power that the weapon can have (power gives projectile speed and range")]
+    private float maxPower = 0;
+
+    private float effectivePower = 0;
+    private bool isChargingPower = false;
+
+    private void Start()
     {
-        
+        Assert.IsTrue(this.chargingTimeInPowerPerSec > 0, "Invalid asset (Charging time value)");
+        Assert.IsTrue(this.maxPower > 0, "Invalid asset (Max power value)");
+        Assert.IsNotNull(this.projectile, "Missing asset (projectile prefab)");
+        Assert.IsNotNull(this.weaponEndPoint, "Missing asset (weapon end point)");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(this.isChargingPower)
+        {
+            this.effectivePower += this.chargingTimeInPowerPerSec * Time.deltaTime;
+            this.effectivePower = Mathf.Clamp(this.effectivePower, 0, this.maxPower);
+        }
+    }
+
+    private void Fire()
+    {
+        Debug.Log("Fire with power: " + this.effectivePower);
+        Instantiate(this.projectile, this.weaponEndPoint);
+        this.isChargingPower = false;
+        this.effectivePower = 0;
+    }
+
+    public void OnInputFire(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Started:
+                // Button pressed
+                this.isChargingPower = true;
+                break;
+            case InputActionPhase.Canceled:
+                // Button released
+                this.Fire();
+                break;
+        }
     }
 }
